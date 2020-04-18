d0edaf7868fbff603bfa0a3ad744d08fa4d56d6f
Derek Holloway <dholloway543@live.com>
2020-04-18 13:25:53 -0700

    _

---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using Mistox;
+
+namespace Renesas_unassembler {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            openFileDialog1.ShowDialog();
+        }
+
+        public static string[] converted;
+        public static List<byte> bytes;
+
+        public static void WorkerThread() {
+            Parallel.For(0, bytes.Count, index => {
+                if (index % 2 == 0) {
+                    byte cur1 = bytes[(index)];
+                    byte cur2 = bytes[(index) + 1];
+                    Thread.Sleep(0);
+                    string bitecode = Convert.ToString(cur1, 2).PadLeft(8, '0') +
+                        Convert.ToString(cur2, 2).PadLeft(8, '0');
+
+                    string A = bitecode.Substring(0, 4);
+                    string B = bitecode.Substring(4, 4);
+                    string C = bitecode.Substring(8, 4);
+                    string D = bitecode.Substring(12, 4);
+                    converted[index/2] = Unassembly.Unassemble(A, B, C, D);
+                }
+            });
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e) {
+            OpenFileDialog self = (OpenFileDialog)sender;
+            string path = self.FileName;
+            bytes = File.ReadAllBytes(path).ToList();
+            converted = new string[bytes.Count/2];
+            Thread x = new Thread(new ThreadStart(WorkerThread));
+            x.Start();
+            x.Join();
+            File.WriteAllText(@"C:\Users\" + Environment.UserName + @"\Desktop\Out.asm", string.Join("\n", converted));
+            Application.ExitThread();
+        }
+    }
+}
---
+++ Unassembly.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mistox {
+    class Unassembly {
+
+        static string Cmd2(string CmdName, string V1, string V2, string Desc) {
+            return CmdName + " " + V1 + ", " + V2 + " ; " + Desc;
+        }
+
+        public static string Unassemble(string Command, string A, string B, string C) {
+
+            // Rn = Register Number
+            // Im = Immediate Number
+            // Pc = Program Counter
+            // Sc = Stack Conter
+            // T = Status register for True / False
+
+
+            string nA = "0x" + Convert.ToString(Convert.ToInt32(A), 16);
+            string nB = "0x" + Convert.ToString(Convert.ToInt32(B), 16);
+            string nC = "0x" + Convert.ToString(Convert.ToInt32(C), 16);
+            string nBC = "0x" + Convert.ToString(Convert.ToInt32(B + C), 16);
+
+            //////////////////////////////////// Data Transfer Instructions ///////////////////////////////////////////////////////////
+
+            if (Command == "1110") {
+                return Cmd2("MOV", nBC, nA, "Data transfer, Immediate data transfer, Peripheral module data transfer, Structure data transfer, Reverse stack transfer");
+
+            }
+
+            //////////////////////////////////// Arithmatic Instructions ///////////////////////////////////////////////////////////
+
+            else if (Command == "0011" && C == "1100") {
+                return Cmd2("ADD", nB, nC, "Addition: ADD Rm, Rn | Rn + Rm => Rn");
+
+            } else if (Command == "0111") {
+                return Cmd2("ADD", nBC, nA, "Addition Immidiate: ADD imm, Rn | imm + Rn => Rn");
+
+            } else if (Command == "0011" && C == "1110") {
+                return Cmd2("ADDC", nB, nA, "ADD Carry: ADDC Rm, Rn | Rn + Rm + T => Rn, carry => T");
+
+            } else if (Command == "0011" && C == "1111") {
+                return Cmd2("ADDV", nB, nA, "ADD Overflow: ADDV Rm, Rn | Rn + Rm => Rn, overflow => T");
+
+            } else if (Command == "1000" && A == "1000") {
+                return Cmd2("CMP/EQ", nBC, "R0", "Is Equal: CMP/EQ Imm | if (Imm == Register0) => T");
+
+            } else if (Command == "0011" && C == "0000") {
+                return Cmd2("CMP/EQ", nB, nA, "Is Equal: CMP/EQ Rm, Rn | if (Rm == Rn) => T");
+
+            } else if (Command == "0011" && C == "0010") {
+                return Cmd2("CMP/HS", nB, nA, "Greater Than: CMP/HS Rm, Rn | if (Rm >= Rm) => T");
+            
+            }
+
+            return "NOP";
+
+        }
+
+    }
+}
